aabd4dafafb730b8ca6ce32e60039bceebe806be
Pablo Vazquez <venomgfx@gmail.com>
2013-08-08 14:37:29 -0300

    Bump version for pre-release

--- addon/scene_amaranth_toolset.py
+++ addon/scene_amaranth_toolset.py
@@ -19,7 +19,7 @@
 bl_info = {
     "name": "Amaranth Toolset",
     "author": "Pablo Vazquez, Bassam Kurdali, Sergey Sharybin",
-    "version": (0, 4, 0),
+    "version": (0, 4, 9),
     "blender": (2, 68),
     "location": "Scene Properties > Amaranth Toolset Panel",
     "description": "A collection of tools and settings to improve productivity",
