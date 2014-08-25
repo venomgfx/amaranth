96e827338bdc52a0e4b9d8c3d9186332475c1ba9
csaez <cesarte@gmail.com>
2014-08-25 00:30:06 -0400

    Preferences migrated

---
+++ __init__.py
@@ -0,0 +1,47 @@
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+# Imports
+import bpy
+
+if "prefs" in locals():
+    import imp
+    imp.reload(prefs)
+else:
+    from . import prefs
+
+# Addon info
+bl_info = {
+    "name": "Amaranth Toolset",
+    "author": "Pablo Vazquez, Bassam Kurdali, Sergey Sharybin, Lukas Tönne",
+    "version": (0, 9, 6),
+    "blender": (2, 71),
+    "location": "Everywhere!",
+    "description": "A collection of tools and settings to improve productivity",
+    "warning": "",
+    "wiki_url": "http://pablovazquez.org/amaranth",
+    "tracker_url": "",
+    "category": "Scene"}
+
+
+# Registration
+def register():
+    bpy.utils.register_class(prefs.Prefs)
+
+
+def unregister():
+    bpy.utils.unregister_class(prefs.Prefs)
+
+if __name__ == "__main__":
+    register()
